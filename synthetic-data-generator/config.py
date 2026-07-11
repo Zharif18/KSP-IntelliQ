d6a6a345b46f9044b766692adc048f07cf8c861a
Zharif18 <mohamedzharif18@gmail.com>
2026-07-11 07:16:53 +0530

    Started Phase 2 by generating synthetic data

---
+++ synthetic-data-generator/config.py
@@ -0,0 +1,31 @@
+"""
+Configuration for Synthetic Data Generator
+"""
+
+# -------------------------
+# Number of Records
+# -------------------------
+
+DISTRICT_COUNT = 31
+
+UNIT_COUNT = 150
+
+EMPLOYEE_COUNT = 300
+
+USER_COUNT = 300
+
+CRIME_COUNT = 5000
+
+PERSON_COUNT = 10000
+
+CRIME_PERSON_COUNT = 12000
+
+EVIDENCE_COUNT = 7000
+
+PREDICTION_COUNT = 500
+
+# -------------------------
+# Output Folder
+# -------------------------
+
+OUTPUT_FOLDER = "output"
